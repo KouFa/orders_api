efe442e5c2f9079e613d663f080f6d6de8783485
Panagiotis Mouratidis <pmouratidis@deloitte.gr>
2023-03-02 18:52:01 +0200

    refactoring

--- orders_api/read.py
+++ orders_api/read.py
@@ -3,11 +3,10 @@ import boto3
 import os
 from boto3.dynamodb.conditions import Key
 
+dynamodb = boto3.resource('dynamodb')
+table_name = os.environ.get('ORDERS_TABLE')
 
-def lambda_handler(event, context):
-    order = {'id': 123, 'itemName': 'Mac Book Pro', 'quantity': 100}
-    dynamodb = boto3.resource('dynamodb')
-    table_name = os.environ.get('ORDERS_TABLE')
+def lambda_handler(event, context): 
     table = dynamodb.Table(table_name)
     order_id = int(event['pathParameters']['id'])
     response = table.query(KeyConditionExpression=Key('id').eq(order_id))
